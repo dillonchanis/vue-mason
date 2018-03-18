b90ba322d8388a4fb7223c5a2713d41c3388cf61
Dillon Chanis <dillonchanis92@gmail.com>
2018-03-18 19:15:46 -0400

    Removing unused function

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,6 @@ const utils = module.exports = {}
 
 utils.compose = (...fns) => x => fns.reduceRight((acc, fn) => fn(acc), x)
 
-utils.componentType = str => str.replace('Component', '')
-
 utils.padLeft = (amt, char) => str => str.padStart((str.length + amt), char)
 
 utils.bullet = str => `• ${str}`
