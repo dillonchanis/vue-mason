312d4ac4e35077262573e0633276b339891be825
Dillon Chanis <dillonchanis92@gmail.com>
2018-04-06 22:15:41 -0400

    Creating capitalize function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
 const bullet = str => `• ${str}`
 
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1)
+
 const compose = (...fns) => x => fns.reduceRight((acc, fn) => fn(acc), x)
 
 const padLeft = (amt, char) => str => str.padStart((str.length + amt), char)
@@ -8,6 +10,7 @@ const removeTrailingSlash = str => str.replace(/\/+$/, '')
 
 module.exports = {
   bullet,
+  capitalize,
   compose,
   padLeft,
   removeTrailingSlash,
